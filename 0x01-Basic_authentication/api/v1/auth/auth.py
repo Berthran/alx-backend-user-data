3eb48c82157b4e1b4a77b8b8ca28d1d595f7491c
Daniel Berthran <berthlearn@gmail.com>
2024-11-12 17:44:07 +0100

    create authentication template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+A class to manage API authentication
+'''
+import requests
+from typing import (List, TypeVar)
+
+
+class Auth:
+    '''
+    A class to manage API authentication
+    '''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks if a API endpoint requires authentication
+        Returns:
+            - False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns a header for authorization
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns a Flask request Object
+        """
+        return None
