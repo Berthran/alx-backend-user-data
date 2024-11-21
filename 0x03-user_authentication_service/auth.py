804ada70360935c9f7189923a7acc626c815d52e
Daniel Berthran <berthlearn@gmail.com>
2024-11-21 15:11:43 +0100

    change shebang

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3 env
+#!/usr/bin/env python3
 '''
 Hash user password
 '''
 
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +15,15 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: bytes) -> User:
+        """Registers a user with email and password
+        """
+
