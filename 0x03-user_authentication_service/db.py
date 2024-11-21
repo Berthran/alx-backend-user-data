b1645be8ac659ba35f7f25f368045be306894479
Daniel Berthran <berthlearn@gmail.com>
2024-11-21 10:28:57 +0100

    create method to add user without authentification

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """ Save the user to the database
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        try:
+            session.add(new_user)
+            session.commit()
+            session.refresh(new_user)
+            return new_user
+        except Exception as e:
+            session.rollback()
