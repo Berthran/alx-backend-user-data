017f2df48d0fd1fbd5cc87988efecb25c730d717
Daniel Berthran <berthlearn@gmail.com>
2024-11-22 03:22:55 +0100

    create route for sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ Basic Flask App
 
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -35,6 +35,24 @@ def users():
         return jsonify({'email': email, 'message': "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Login registerd users
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({"error": "Missing email or password"}), 400
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = make_response()
+        response.set_cookie("session_id", session_id)
+        return jsonify({"email": email, "message": "logged in"})
 
 
 if __name__ == "__main__":
